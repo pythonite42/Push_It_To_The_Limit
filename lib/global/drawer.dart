43354d39500586a1c2c7bae91f7409e2efe5eef0
sarahLoeffler <sarah.loeffler42@gmail.com>
2022-06-01 22:17:31 +0200

    drawer

--- lib/global/drawer.dart
+++ lib/global/drawer.dart
@@ -1,9 +1,20 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
-import 'package:pushit/global/list_tiles.dart';
+import 'package:pushit/colors.dart';
+import 'package:pushit/global/appbar.dart';
+import 'package:pushit/global/global_widgets.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/services.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({Key? key}) : super(key: key);
+
+  Future<List> getData() async {
+    ByteData bytes = await rootBundle.load('assets/pushit_logo.png');
+    Uint8List list = bytes.buffer.asUint8List();
+    return [list, "Sarah", "Kawasaki Z400"];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,9 +25,7 @@ class MyDrawer extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Column(children: [
-                    SizedBox(
-                      height: 100,
-                    ),
+                    SizedBox(height: MyAppBar().preferredSize.height * 2),
                     MyTile(
                       context: context,
                       heightFactor: 0.2,
@@ -27,12 +36,113 @@ class MyDrawer extends StatelessWidget {
                         "assets/pushit_logo.png",
                       ),
                     ),
-                    SizedBox(
-                      height: 20,
+                    SpaceH(),
+                    FutureBuilder<List>(
+                        future: getData(),
+                        builder: (context, AsyncSnapshot<List> snapshot) {
+                          if (snapshot.hasData) {
+                            return ListTile(
+                              contentPadding: EdgeInsets.only(left: 30),
+                              title: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  CircleAvatar(
+                                    radius: MySize(context).w * 0.1,
+                                    backgroundImage:
+                                        MemoryImage(snapshot.data![0]), //here
+                                  ),
+                                  SpaceW(),
+                                  Expanded(
+                                      child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                        Text(
+                                          snapshot.data![1],
+                                          style: TextStyle(
+                                              color: Theme.of(context)
+                                                  .colorScheme
+                                                  .onPrimary),
+                                        ),
+                                        Text(snapshot.data![2],
+                                            style: TextStyle(
+                                                color: Theme.of(context)
+                                                    .colorScheme
+                                                    .onPrimary))
+                                      ])),
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(Icons.settings,
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .onPrimary))
+                                ],
+                              ),
+                              onTap: () {},
+                            );
+                          } else if (snapshot.hasError) {
+                            return ListTile(
+                              contentPadding:
+                                  EdgeInsets.symmetric(horizontal: 30),
+                              title: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: <Widget>[
+                                    Icon(
+                                      Icons.error_outline,
+                                      color: red,
+                                      size: MySize(context).h * 0.08,
+                                    ),
+                                    SpaceW(),
+                                    Text(
+                                      "Server-Fehler",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .onPrimary),
+                                    ),
+                                  ]),
+                              onTap: () {},
+                            );
+                          } else {
+                            return ListTile(
+                              contentPadding:
+                                  EdgeInsets.symmetric(horizontal: 30),
+                              title: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: <Widget>[
+                                    SizedBox(
+                                      width: MySize(context).h * 0.08,
+                                      height: MySize(context).h * 0.08,
+                                      child:
+                                          CircularProgressIndicator(color: red),
+                                    ),
+                                    SpaceW(),
+                                    Text(
+                                      "Loading ...",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .onPrimary),
+                                    ),
+                                  ]),
+                              onTap: () {},
+                            );
+                          }
+                        }),
+                    SpaceH(0.03),
+                    Divider(
+                      thickness: 1,
+                      color: Theme.of(context).colorScheme.onPrimary,
                     ),
                     MyTile(
                         context: context,
-                        heightFactor: 0.2,
+                        heightFactor: 0.1,
                         onTap: () async {
                           SharedPreferences prefs =
                               await SharedPreferences.getInstance();
@@ -50,46 +160,24 @@ class MyDrawer extends StatelessWidget {
                       thickness: 1,
                       color: Theme.of(context).colorScheme.onPrimary,
                     ),
-                    ConstrainedBox(
-                        constraints: BoxConstraints(
-                          minHeight: 100, //Size(context).h * 0.67,
-                        ),
-                        child: Column(children: [
-                          Container(
-                            height: 50, //Size(context).h * heightFactor,
-                            child: ListTile(
-                              contentPadding:
-                                  EdgeInsets.symmetric(horizontal: 30),
-                              title: Text(
-                                "Über Push It To The Limit",
-                                style: TextStyle(
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .onPrimary),
-                              ),
-                              onTap: () {
-                                Navigator.pop(context);
-                              },
-                            ),
-                          ),
-                          Container(
-                            height: 50, //Size(context).h * heightFactor,
-                            child: ListTile(
-                              contentPadding:
-                                  EdgeInsets.symmetric(horizontal: 30),
-                              title: Text(
-                                "Administrator kontaktieren",
-                                style: TextStyle(
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .onPrimary),
-                              ),
-                              onTap: () {
-                                Navigator.pop(context);
-                              },
-                            ),
-                          ),
-                        ]))
+                    SpaceH(0.02),
+                    MyTile(
+                      context: context,
+                      heightFactor: 0.05,
+                      text: "Über Push It To The Limit",
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                    SpaceH(0.02),
+                    MyTile(
+                      context: context,
+                      heightFactor: 0.05,
+                      text: "Administrator kontaktieren",
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                    ),
                   ]),
                 ])));
   }
--- lib/global/global_widgets.dart
+++ lib/global/global_widgets.dart
@@ -63,3 +63,40 @@ class Heading extends StatelessWidget {
     );
   }
 }
+
+class MyTile extends StatelessWidget {
+  const MyTile(
+      {Key? key,
+      this.text = "",
+      required this.context,
+      this.heightFactor = 0.13,
+      this.child = const Text(""),
+      required this.onTap})
+      : super(key: key);
+  final String text;
+  final BuildContext context;
+  final double heightFactor;
+  final Widget child;
+  final Function onTap;
+  @override
+  Widget build(BuildContext context) {
+    var widget = child;
+    if (text != "") {
+      widget = Text(
+        text,
+        style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+      );
+    }
+    return SizedBox(
+        height: MySize(context).h * heightFactor,
+        child: Center(
+          child: ListTile(
+            contentPadding: EdgeInsets.symmetric(horizontal: 30),
+            title: widget,
+            onTap: () {
+              onTap();
+            },
+          ),
+        ));
+  }
+}
