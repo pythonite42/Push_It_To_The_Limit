3287030727f648d5121f2520de11fa46f7fd5a7f
sarahLoeffler <sarah.loeffler42@gmail.com>
2022-06-06 23:03:48 +0200

    connect to pushit database

--- lib/sql.dart
+++ lib/sql.dart
@@ -3,7 +3,7 @@ import 'package:postgres/postgres.dart';
 
 class SQL {
   openConnection() async {
-    var connection = PostgreSQLConnection("185.228.138.208", 2345, "postgres",
+    var connection = PostgreSQLConnection("185.228.138.208", 2345, "pushit",
         username: "pushit", password: '#gZxwwavt0o4!IM7');
     await connection.open();
     log("connection done");
